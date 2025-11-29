f66c1f5f06f61eddb627da38ce1a58aea69dd144
Muhammad Yahaya <adelekeyahaya05@gmail.com>
2025-11-29 11:58:32 +0100

    update: login endpoint url

--- routes/api.php
+++ routes/api.php
@@ -2,10 +2,10 @@
 
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
-use App\Http\Controllers\Api\AuthController;
 use App\Http\Controllers\Api\MemberController;
 use App\Http\Controllers\Api\UpvoteController;
 use App\Http\Controllers\Api\DownvoteController;
+use App\Http\Controllers\Api\Auth\AuthController;
 
 Route::get('/user', function (Request $request) {
     return $request->user();
@@ -19,9 +19,9 @@ Route::get('/', function (Request $request) {
     ]);
 });
 
+Route::post('auth/login', [AuthController::class, 'loginWithGoogle'])->middleware('throttle:5,1');
 Route::get('members', [MemberController::class, 'index']);
 Route::get('members/{id}', [MemberController::class, 'show']);
 Route::post('upvote-member', UpvoteController::class);
 Route::post('downvote-member', DownvoteController::class);
-Route::post('login', [AuthController::class, 'loginWithGoogle'])->middleware('throttle:5,1');
 
